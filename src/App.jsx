25b22d56193a65ab5b8a1155e902381f40339c18
Muhammad Hidayat Bin Salaman <124369107+rawrisotto@users.noreply.github.com>
2024-02-18 18:48:37 +0000

    refactor: Personalized portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Hidayat Bin Salaman",
+  title: "Web Developer",
+  email: "kudahidayat8@gmail.com",
+  gitHub: "rawrisotto",
+  instagram: "",
+  linkedIn: "Muhammad Hidayat Bin Salaman",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,22 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at University of Wollongong. I am currently learning to create wonderful and beautiful web applications.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "HTML",
+  "CSS",
+  "React",
+  "JavaScript",
+  "TypeScript",
+  "Express",
+  "MongoDB",
+  "MySQL",
+  "PHP",
 ];
 
 /**
@@ -44,8 +47,10 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
+// const detailOrQuote =
+//   "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about learning new things. I may not have a lot under my repository right now but I hope to build it up, 1 day at a time."
 
 const About = () => {
   return (
